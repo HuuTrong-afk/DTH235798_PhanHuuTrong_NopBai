2e6054a0c7b59def4642938fe02107622c4b08c8
HuuTrong <htphan0122@gmail.com>
2025-09-08 09:43:57 +0700

    Chuong2_Bai1

---
+++ Chuong2-Bai3.py
@@ -0,0 +1,6 @@
+toan=float(input("Nhập điểm Toán dốt nát của mày vào:"))
+ly=float(input("Nhập điểm lý thảm hại của mày vào:"))
+hoa=float(input("Nhập điểm hóa cận bả của mày vào:"))
+dtb=(toan+ly+hoa)/3
+print("Điểm trung bình học thức của mày =",dtb)
+print("Điểm làm tròn để cứu vớt cuộc đời mày =",round(dtb,2))
