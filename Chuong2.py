fad0da77dadee28e082d264a02702f9e2f10e736
HuuTrong <htphan0122@gmail.com>
2025-09-08 09:07:43 +0700

    Chương2

---
+++ Chuong2.py
@@ -0,0 +1,9 @@
+import math
+try: 
+    r = float (input("Địt mẹ mày nhập bán kính đường tròn vào: "))
+    cv = 2*math.pi*r
+    dt = r**2
+    print("Chu vi của mày bằng: ",cv)
+    print("diện tích là: ", dv)
+except:
+    print("lỗi rồi!")
